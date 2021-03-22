b37f6319a27f76f4e5a16f11f83a0664c7e24775
Alberto Planas <aplanas@suse.com>
2021-03-22 17:51:31 +0100

    Remove flycheck warning

--- build.sh
+++ build.sh
@@ -417,6 +417,9 @@ function configure {
 
 (setq load-path (cons "~/.emacs.d/lisp" load-path))
 
+;; Remove flymake warning message
+(remove-hook 'flymake-diagnostic-functions 'flymake-proc-legacy-flymake)
+
 ;; SLIME
 ;; (setq inferior-lisp-program "~/bin/sbcl") ; your Lisp system
 ;; (add-to-list 'load-path "~/bin/emacs-cvs/slime/")  ; your SLIME directory
